a734062c5e79e4101db392014a2ddb664b17209b
Nanchen Yang <ncyang@thoughtworks.com>
2018-06-21 17:31:21 +0800

    Initial Commit

---
+++ ios-workshop-git-news/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ios-workshop-git-news
+//
+//  Created by Nanchen
+Yang on 2018/6/21.
+//  Copyright © 2018 Nanchen
+Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
